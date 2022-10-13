7d558cf8286e75f59e14f14d60a8e9cd07f10848
Georgy Guminov <gogagum@gmail.com>
2022-10-14 00:12:42 +0300

    Transpose.

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,42 @@
-__kernel void matrix_transpose(...)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.h>
+#endif
+
+#line 6
+
+#ifndef WORKGROUP_SIZE
+#define WORKGROUP_SIZE 16
+#endif
+
+__kernel void matrix_transpose_simple(__global const float* data,
+                                      __global       float* data_out,
+                                      unsigned int M,
+                                      unsigned int K)
 {
-    // TODO
+  unsigned int global_i = get_global_id(0);
+  unsigned int global_j = get_global_id(1);
+
+  if (global_i < M && global_j < K) {
+    unsigned int local_i = get_local_id(0);
+    unsigned int local_j = get_local_id(1);
+    data_out[global_j * M + global_i] = data[global_i * K + global_j];
+  }
+}
+
+__kernel void matrix_transpose(__global const float* data,
+                               __global       float* data_out,
+                               unsigned int M,
+                               unsigned int K)
+{
+    unsigned int global_i = get_global_id(0);
+    unsigned int global_j = get_global_id(1);
+
+    __local float working_part[WORKGROUP_SIZE][WORKGROUP_SIZE];
+
+    if (global_i < M && global_j < K) {
+      unsigned int local_i = get_local_id(0);
+      unsigned int local_j = get_local_id(1);
+      working_part[local_i][local_j] = data[global_i * K + global_j];
+      data_out[global_j * M + global_i] = working_part[local_i][local_j];
+    }
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -27,38 +27,94 @@ int main(int argc, char **argv)
     std::vector<float> as_t(M*K, 0);
 
     FastRandom r(M+K);
-    for (unsigned int i = 0; i < as.size(); ++i) {
-        as[i] = r.nextf();
+    for (float& a : as) {
+        a = r.nextf();
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
 
     as_gpu.writeN(as.data(), M*K);
 
-    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
+    ocl::Kernel matrix_transpose_kernel(matrix_transpose,
+                                        matrix_transpose_length,
+                                        "matrix_transpose");
     matrix_transpose_kernel.compile();
+
+    ocl::Kernel matrix_transpose_simple_kernel(matrix_transpose,
+                                               matrix_transpose_length,
+                                               "matrix_transpose_simple");
+
+    matrix_transpose_simple_kernel.compile();
+
+    {
+      timer t;
+      for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        unsigned int work_group_size = 16;
+        unsigned int global_work_size = 16;
+        // Для этой задачи естественнее использовать двухмерный NDRange.
+        // Чтобы это сформулировать в терминологии библиотеки - нужно
+        // вызвать другую вариацию конструктора WorkSize.
+        // В CLion удобно смотреть какие есть вариант аргументов в
+        // конструкторах: поставьте каретку редактирования кода внутри
+        // скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4
+        // и 6 параметров
+        // - для 1D, 2D и 3D рабочего пространства соответственно
+        matrix_transpose_simple_kernel.exec(gpu::WorkSize(work_group_size,
+                                                          global_work_size,
+                                                          M, K),
+                                            as_gpu, as_t_gpu, M, K);
+
+        t.nextLap();
+      }
+      std::cout << "GPU (simple): " << t.lapAvg() << "+-" << t.lapStd()
+                << " s" << std::endl;
+      std::cout << "GPU (simple): " << M*K/1000.0/1000.0 / t.lapAvg()
+                << " millions/s" << std::endl;
+    }
+
+    as_t_gpu.readN(as_t.data(), M*K);
+
+    // Проверяем корректность результатов
+    for (int j = 0; j < M; ++j) {
+      for (int i = 0; i < K; ++i) {
+        float a = as[j * K + i];
+        float b = as_t[i * M + j];
+        if (a != b) {
+          std::cerr << "Not the same!" << std::endl;
+          return 1;
+        }
+      }
+    }
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
-            // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
-            // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
-            // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
+            unsigned int work_group_size = 16;
+            unsigned int global_work_size = 16;
+            // Для этой задачи естественнее использовать двухмерный NDRange.
+            // Чтобы это сформулировать в терминологии библиотеки - нужно
+            // вызвать другую вариацию конструктора WorkSize.
+            // В CLion удобно смотреть какие есть вариант аргументов в
+            // конструкторах: поставьте каретку редактирования кода внутри
+            // скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4
+            // и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size,
+                                                       global_work_size,
+                                                       M,
+                                                       K),
+                                         as_gpu, as_t_gpu, M, K);
 
             t.nextLap();
         }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd()
+                  << " s" << std::endl;
+        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg()
+                  << " millions/s" << std::endl;
     }
 
     as_t_gpu.readN(as_t.data(), M*K);
@@ -74,7 +130,5 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
-
     return 0;
 }
