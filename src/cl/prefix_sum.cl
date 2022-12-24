2d6ee5cc7e6c42e856ed3211f36cb6626c3e22ca
Georgy Guminov <gogagum@gmail.com>
2022-12-24 05:02:50 +0300

    Prefix.

--- src/cl/prefix_sum.cl
+++ src/cl/prefix_sum.cl
@@ -1,1 +1,31 @@
-// TODO
+//----------------------------------------------------------------------------//
+__kernel void
+reduce2(__global const unsigned int* currReduced,
+        __global       unsigned int* res,
+        unsigned int n) {
+    const unsigned int id = get_global_id(0);
+
+    if (id * 2 + 1 < n) {
+        res[id] = currReduced[id * 2] + currReduced[id * 2 + 1];
+    } else if (id * 2 < n) {
+        res[id] = currReduced[id * 2];
+    } else if (id < n) {
+        res[id] = 42;
+    }
+}
+
+//----------------------------------------------------------------------------//
+__kernel void
+prefix_sum_impl(__global const unsigned int* currReduced,
+                __global       unsigned int* ret,
+                unsigned int stage,
+                unsigned int size) {
+    const unsigned int id = get_global_id(0);
+    const unsigned int i = (id + 1) >> stage;
+
+    if (id <= size) {
+        if (i & 1 == 1) {
+            ret[id] += currReduced[i-1];
+        }
+    }
+}
--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -3,9 +3,13 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <iostream>
+#include <limits>
+#include <vector>
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/prefix_sum_cl.h"
+#include "libgpu/work_size.h"
 
 
 template<typename T>
@@ -22,13 +26,21 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
-	int benchmarkingIters = 10;
-	unsigned int max_n = (1 << 24);
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
-	for (unsigned int n = 2; n <= max_n; n *= 2) {
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+	int benchmarkingIters = 10;
+    unsigned int max_n = (1 << 20);
+
+    for (unsigned int n = 2; n <= max_n; n *= 2) {
 		std::cout << "______________________________________________" << std::endl;
 		unsigned int values_range = std::min<unsigned int>(1023, std::numeric_limits<int>::max() / n);
 		std::cout << "n=" << n << " values in range: [" << 0 << "; " << values_range << "]" << std::endl;
+
+        //n += 3;  // Проверка, что случай не 2^n работает ОК.
 
 		std::vector<unsigned int> as(n, 0);
 		FastRandom r(n);
@@ -76,8 +88,60 @@ int main(int argc, char **argv)
 			std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
 		}
 
-		{
-			// TODO: implement on OpenCL
+        std::vector<unsigned int> gpuResult(n, 0);
+
+        {
+            gpu::gpu_mem_32u prefixSumsGPU;
+            prefixSumsGPU.resizeN(n);
+            prefixSumsGPU.writeN(as.data(), n);
+            gpu::gpu_mem_32u currReduced;
+            currReduced.resizeN(n);
+            gpu::gpu_mem_32u tmpReduced;
+            tmpReduced.resizeN(n);
+
+            ocl::Kernel reduce2(prefix_sum_kernel,
+                                prefix_sum_kernel_length,
+                                "reduce2");
+            reduce2.compile();
+
+            ocl::Kernel prefixSumImpl(prefix_sum_kernel,
+                                      prefix_sum_kernel_length,
+                                      "prefix_sum_impl");
+            prefixSumImpl.compile();
+
+            const auto calcWorkSize = [](unsigned int sz) {
+                const unsigned int workGroupSize = 128;
+                auto globalSize = (sz + workGroupSize - 1) / workGroupSize * workGroupSize;
+                return gpu::WorkSize(workGroupSize, globalSize);
+            };
+
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                currReduced.writeN(as.data(), n);
+                prefixSumsGPU.writeN(std::vector<unsigned int>(n, 0).data(), n);
+
+                for (unsigned int stage = 0; ((n + 1) >> stage) > 0; ++stage) {
+                    reduce2.exec(calcWorkSize((n + 1) >> stage), currReduced, tmpReduced, n);
+
+                    prefixSumImpl.exec(calcWorkSize(n), currReduced, prefixSumsGPU,
+                                       stage, n);
+                    currReduced.swap(tmpReduced);
+                }
+
+                t.nextLap();
+            }
+
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+
+            prefixSumsGPU.readN(gpuResult.data(), n);
 		}
+
+        // Проверяем корректность результатов
+        for (int i = 0; i < n; ++i) {
+            EXPECT_THE_SAME(bs[i], gpuResult[i], "GPU results should be equal to CPU results!");
+        }
+
 	}
 }
